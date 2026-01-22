dee6bc1479f35d65b56a47653d12d57a17145714
Nandhini <archontech226@gmail.com>
2026-01-22 14:36:07 +0530

    Update urls.py

--- coorider/urls.py
+++ coorider/urls.py
@@ -1,1 +1,9 @@
 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),                 # Laptop dashboard
+    path('api/update-location/', views.update_location, name='update_location'),  # Phone sends GPS
+    path('api/get-locations/', views.get_locations, name='get_locations'),        # Laptop fetches live data
+]
