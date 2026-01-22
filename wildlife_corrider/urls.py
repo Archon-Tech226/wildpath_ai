889eed52e5ed70e5b4511b74d3df9b77aaae37d4
Nandhini <archontech226@gmail.com>
2026-01-22 16:08:00 +0530

    Fix typo in include path for coorider.urls

--- wildlife_corrider/urls.py
+++ wildlife_corrider/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('cooridor.urls')),   # all our app URLs
+    path('', include('coorider.urls')),   # all our app URLs
 ]
 
