07b5e714b1b1095c0dcdb869eada8b4903306b3e
Nandhini <archontech226@gmail.com>
2026-01-22 14:33:21 +0530

    Add AnimalLocation model for tracking animal positions

--- coorider/models.py
+++ coorider/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+class AnimalLocation(models.Model):
+    animal_id = models.CharField(max_length=50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    timestamp = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.animal_id} @ ({self.latitude}, {self.longitude})"
 
