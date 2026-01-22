d8f982d0c4bcb9f9f0df0f4a21a5cb865d3bab3c
Nandhini <archontech226@gmail.com>
2026-01-22 14:47:19 +0530

    Add map setup and dynamic animal markers
    Implement map initialization and animal location fetching.

--- static files/js/main.js
+++ static files/js/main.js
@@ -1,1 +1,32 @@
+// Initialize map
+var map = L.map('map').setView([11.0, 76.9], 12);  // Center coordinates
+
+// Load OpenStreetMap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19
+}).addTo(map);
+
+// Keep track of animal markers
+var markers = {};
+
+// Fetch animal locations every 3 seconds
+setInterval(function() {
+    fetch('/api/get-locations/')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(a => {
+                if (markers[a.animal_id]) {
+                    // Move existing marker
+                    markers[a.animal_id].setLatLng([a.latitude, a.longitude])
+                                         .bindPopup(a.animal_id);
+                } else {
+                    // Create new marker
+                    markers[a.animal_id] = L.marker([a.latitude, a.longitude])
+                                             .addTo(map)
+                                             .bindPopup(a.animal_id);
+                }
+            });
+        })
+        .catch(err => console.error("Error fetching locations:", err));
+}, 3000);
 
