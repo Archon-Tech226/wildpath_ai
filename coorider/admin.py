6d8428e3212ba9f263aefad0612a84a252c336a3
Nandhini <archontech226@gmail.com>
2026-01-22 14:30:50 +0530

    Update admin.py

--- coorider/admin.py
+++ coorider/admin.py
@@ -1,1 +1,9 @@
+from django.contrib import admin
+from .models import AnimalLocation
+
+@admin.register(AnimalLocation)
+class AnimalLocationAdmin(admin.ModelAdmin):
+    list_display = ('animal_id', 'latitude', 'longitude', 'timestamp')
+    list_filter = ('animal_id',)
+    search_fields = ('animal_id',)
 
